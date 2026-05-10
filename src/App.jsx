e9b02d97051d2a88fa13d0a9e242d6ef5e1e85c7
Korbinian-iu <korbiheiland@gmail.com>
2026-05-10 14:15:21 +0200

    Update App.jsx
    alle Daten werden erst hier in der App importiert, die Tabellendaten werden nicht schon in der Tabelle importiert

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button, Table, Container } from "react-bootstrap";
 import MyNavbar from "./components/MyNavbar";
 import CO2Table from "./components/CO2Table";
+import emissionsData from "./data/emissionsData";
 
 function App() {
+  const [filderedData, setFilteredData] = useState(tableData);
+  const handleSearch = (searchTerm) => {
+    const results = tableData.filter((item) =>
+      Object.values(item).some((val) =>
+        String(val).toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+    setFilteredData(results); {*Update löst Neu-Rendern der Tabelle aus*}
+  };
   return (
     <>
     <div className="App">
@@ -12,10 +22,12 @@ function App() {
         <h1>CO2 Footprint</h1>
       </header>
       <MyNavbar/>
+      <SearchBar onSearch={handleSearch} />
       </div>
       <main className="container">
         <p>Hier sind die aktuellen Daten:</p>
-        <CO2Table/>
+        {/* Die Tabelle erhält NUR noch die gefilterten Daten als Prop */}
+        <CO2Table data={filteredData} />
       </main>
     
     </div>
